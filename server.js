d08ef07340b042ddd6799221a11c8cc40cb9e6de
Joshua May <jmay89@icloud.com>
2017-08-18 12:51:13 -0400

    working robot project

--- server.js
+++ server.js
@@ -1,36 +1,58 @@
 const express = require('express');
 const app = express();
 const mustacheExpress = require('mustache-express');
-const robots = require('./data');
-const dal = require('./dal');
-const robotArray = robots.users;
-
 // Register '.mustache' extension with The Mustache Express
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
+app.set('views', './views');
+
+const robotData = require('./public/data.js');
+// pulls data sucessfully
+//objects users with array of objects inside
+
+const robotArray = robotData.users;
+//console.log(robotArray);
+//just an array of objects
+
 app.use(express.static('public'));
 //files that don't change can be served in this middleware above
 //put into your public directory and serve from there
 //these are made available to the public via express.static
 
-app.get('/home') function(request, response) {
-  console.log(robotArray);
-  response.render('index', { users: robotArray});
+app.get('/', function(req, res) {
+      res.render('robotPage', {
+        users: robotData.users
+      });
+    });
+      /* first feed is robotPage and that will always be the page it is going to - second feed is content based this is an object for example:
+ });
   /* above sends in an object  -- always send in an object when using mustache */
-});
 
-app.get('/home/:id') function (request, response) {
-  const="chosenBot" = dal.robots.(request.params.id)
-  if(chosenBot.id) {
-    response.render('robotDetail', chosenBot)
-  } else {
-    response.send('no bots bro')
-  }
-})
-// this is not finished above
+      app.get('/:id', function(req, res) {
+        let chosenRobot = {};
+        for (let i = 0; i < robotArray.length; i++) {
 
-app.listen(3000, function () {
-  console.log('Application has started at port 3000')
-})
-// above allows app to run on port 3k and confirms via console log
+          if (robotArray[i].id === +req.params.id) {
+            chosenRobot = robotArray[i]
+          }
+        }
+        console.log(chosenRobot);
+      res.render('indexRobot', chosenRobot)
+      });
+
+//       app.get('/kittehs/:id', function (request, response) {
+//   const chosenKitty = kittyDal.getKitty(request.params.id)
+//   if (chosenKitty.id) {
+//     response.render('kittyDetail', chosenKitty)
+//   } else {
+//     response.send('NO KITTEHS!!!')
+//   }
+// })
+
+      // above pulls individual
+
+
+      app.listen(3000, function() {
+        console.log('Application has started at port 3000')
+      });
+      // above allows app to run on port 3k and confirms via console log
